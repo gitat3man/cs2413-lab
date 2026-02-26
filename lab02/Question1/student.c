5ebd4eff17cf05683c610082161737496ffc5d78
gitat3man <shalomdavid2005@gmail.com>
2026-02-26 14:38:54 -0600

    test

--- lab02/Question1/student.c
+++ lab02/Question1/student.c
@@ -53,7 +53,7 @@ bool isValid(const char *s) {
     //
     // Note:
     // - Input contains only bracket characters, per the prompt.
-
+    int i =0;
     (void)s; // remove after implementing
     return false; // placeholder
 }
